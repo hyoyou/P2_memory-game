89e2a9457e47f53fb792e44c0a9a0649c9d10d75
Heather You <heatherhyou@gmail.com>
2018-05-31 14:58:35 -0400

    Add counter

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+*** Creating the Deck ***
+ *  Display the cards on the page
+ *  Shuffle the list of cards using the provided "shuffle" method below
+ *  Loop through each card and create its HTML
+ *  Add each card's HTML to the page
 */
 
 let deck = document.querySelector('.deck');
@@ -14,21 +15,18 @@ let symbols = ["fa-bolt", "fa-bolt",
               "fa-diamond", "fa-diamond",
               "fa-bicycle", "fa-bicycle",
               "fa-paper-plane-o", "fa-paper-plane-o"]
-let shuffledSymbols = shuffle(symbols);
 
 function cardHtml(card) {
   return `<li class="card"><i class="fa ${card}"></i></li>`;
 }
 
 function generateDeck() {
-  let newDeck = shuffledSymbols.map(function(card) {
+  let newDeck = shuffle(symbols).map(function(card) {
     return cardHtml(card);
   })
 
   deck.innerHTML = newDeck.join('');
 }
-
-generateDeck();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -45,54 +43,71 @@ function shuffle(array) {
     return array;
 }
 
+function initialize() {
+  generateDeck();
+}
+
+initialize();
+
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+*** Setting Up Game Play ***
+ * Set up the event listener for a card. If a card is clicked:
+ * Display the card's symbol (put this functionality in another function that you call from this one)
+ * Add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ * If the list already has another card, check to see if the two cards match
+ *  - if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *  - if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *  - increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *  - if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+*/
 
-// Create a list that holds all of your cards
 let cards = document.querySelectorAll('.card')
-let flippedCards = [];
+let openCards = [];
 let matchedCards = [];
+let counter = 1;
+let counterDisplay = document.querySelector('.moves')
 
+function checkMatch() {
+  if (openCards.length == 2) {
+    // Check for a match
+    let card1 = openCards[0].querySelector('i').classList.value;
+    let card2 = openCards[1].querySelector('i').classList.value;
+
+    if (card1 === card2) {
+      matchedCards.push(openCards[0]);
+      matchedCards.push(openCards[1]);
+      matchedCards.forEach(function(card) {
+        card.classList.add('match');
+        card.classList.remove('open', 'show');
+      });
+      counter += 1;
+      openCards = [];
+    } else {
+    // Place back in deck if not a match
+      setTimeout(function() {
+        cards.forEach(function(card) {
+          card.classList.remove('open', 'show')
+        });
+        counter += 1;
+        openCards = [];
+      }, 500);
+    }
+  }
+}
+
+function openCard(card) {
+  if (openCards.length < 2 && !card.classList.contains('open') && !card.classList.contains('match')) {
+    openCards.push(card);
+    card.classList.add('open', 'show');
+
+    checkMatch();
+  }
+}
+
+// Event Listener
 cards.forEach(function(card) {
   card.addEventListener('click', function(event) {
-    if (flippedCards.length < 2 && !card.classList.contains('open') && !card.classList.contains('match')) {
-      flippedCards.push(card);
-      card.classList.add('open', 'show');
-
-      if (flippedCards.length == 2) {
-        // Check for a match
-        let card1 = flippedCards[0].querySelector('i').classList.value;
-        let card2 = flippedCards[1].querySelector('i').classList.value;
-
-        if (card1 === card2) {
-          matchedCards.push(flippedCards[0])
-          matchedCards.push(flippedCards[1])
-          console.log(matchedCards)
-          matchedCards.forEach(function(card) {
-            card.classList.add('match');
-            card.classList.remove('open', 'show');
-          })
-          flippedCards = [];
-        } else {
-        // Place back in deck if not a match
-          setTimeout(function() {
-            cards.forEach(function(card) {
-              card.classList.remove('open', 'show')
-            });
-            flippedCards = [];
-          }, 500);
-        }
-      }
-    }
+    openCard(card);
+    counterDisplay.innerText = counter;
   })
 })
-
-// initialize();
